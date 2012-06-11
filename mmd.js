6538279a4692f1b9c8469434749a2e39a5e9c104
Alex Lawrence <info@alex-lawrence.com>
2012-06-11 21:22:57 +0200

    Updated to newer version https://github.com/alexlawrence/mmd/issues/1 Added some basic specs

--- mmd.js
+++ mmd.js
@@ -1,1 +1,1 @@
-function define(a,b,c){define[a]=require(c?b:[],c||b)}function require(a,b,c,i){for(c=[],i=0;a[i];)c[i]=define[a[i++]];return b.apply(0,c)}
+function define(a,b,c){define[a]=require(c?b:[],c||b)}function require(a,b,c,i){c=[];for(i in a)c[i]=define[a[i]];return b.apply(0,c)}
